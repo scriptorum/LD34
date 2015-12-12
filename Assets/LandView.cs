f129ab86b04b2e86d8b36e9d9a184ac198795aed
Eric Lund <github@egotron.com>
2015-12-12 03:17:18 -0500

    Really basic beginning. Got a planet. Got a plant. Got rotation. Gotta sleep.

---
+++ Assets/LandView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spewnity;
+
+public class LandView : MonoBehaviour {
+	public float rotateSpeed = 20.0f;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void OnRotatePlanet(InputEvent e)
+	{
+		transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime * e.axis.x);
+//		Debug.Log(e.axis.x);
+	}
+}
