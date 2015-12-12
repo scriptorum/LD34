fdc8d1823d50ee144a86687bc3e5fec04ddfea67
Eric Lund <github@egotron.com>
2015-12-12 17:35:51 -0500

    Cleaned up some prefabs

---
+++ Assets/Scripts/Air.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Air : MonoBehaviour {
+	public AirborneObject cloudPrefab;
+	public AirborneObject rainPrefab;
+
+	void Awake () 
+	{
+	}
+
+	void Start () 
+	{
+
+	}
+
+	void Update () {
+	
+	}
+}
