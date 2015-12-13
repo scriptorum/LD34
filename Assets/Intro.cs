afd1ce019f4a59244e0c87e06bcd27d5e50b0ff2
Eric Lund <github@egotron.com>
2015-12-12 22:00:18 -0500

    Added intro menu

---
+++ Assets/Intro.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour
+{
+	private static float SMALL = 1.5f;
+	private static float LARGE = 1.51f;
+	private static float ROTSPEED = 10f;
+	private static float BOUNCESPEED = 0.3f;
+
+	private float startScale = 1.0f;
+	private float endScale = 1.0f;
+	private float totalTime = 0f;
+	private float remainingTime = 0f;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		remainingTime -= Time.deltaTime;
+		if(remainingTime <= 0)
+		{
+			totalTime = remainingTime = BOUNCESPEED;
+			startScale = endScale;
+			endScale = endScale >= LARGE ? SMALL : LARGE;
+		}
+
+		float curScale = endScale - (endScale - startScale) * (remainingTime / totalTime);
+		transform.localScale = new Vector3(curScale, curScale, 1);
+
+		transform.Rotate(new Vector3(0, 0, ROTSPEED * Time.deltaTime));
+	}
+}
---
+++ Assets/IntroStartButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class IntroStartButton : MonoBehaviour 
+{
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Space))
+			transition();
+	}
+
+	public void OnMouseDown()
+	{
+		transition();
+	}
+
+	public void transition()
+	{
+		SceneManager.LoadScene("Play");
+	}
+}
